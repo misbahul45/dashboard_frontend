1f42133b5d8f03d12b19077f3e8dad9d700d32b4
misbahul45 <misbahulmuttaqin395@gmail.com>
2024-02-15 10:07:21 +0700

    push

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import { LightModeOutlined, DarkModeOutlined, Menus as MenuIcon, Search, SettingsOutlined, ArrowDropDownCircleOutlined } from "@mui/icons-material"
+import FlexBetween from "./FlexBetween"
+const Navbar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <p>Hallo</p>
+    </div>
+  )
+}
+
+export default Dashboard

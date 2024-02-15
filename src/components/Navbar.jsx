399db03c1fc5ccc430a5b51f4aa3c4361b9e86f2
misbahul45 <misbahulmuttaqin395@gmail.com>
2024-02-15 10:07:58 +0700

    psh

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { LightModeOutlined, DarkModeOutlined, Menus as MenuIcon, Search, SettingsOutlined, ArrowDropDownCircleOutlined } from "@mui/icons-material"
+import { LightModeOutlined, DarkModeOutlined, Menu as MenuIcon, Search, SettingsOutlined, ArrowDropDownCircleOutlined } from "@mui/icons-material"
 import FlexBetween from "./FlexBetween"
 const Navbar = () => {
   return (
